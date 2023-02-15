3a0a682ba5833a8d78903c1b58f79255e861ace0
zel-bagh <zakaria.baghazaoui@gmail.com>
2023-02-15 22:54:48 +0100

    Last version

--- srcs/requirements/mariadb/mariadb.sh
+++
@@ -1,5 +0,0 @@
-service mysql start
-mysql -u root -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASSWORD'"
-mysql -u root -e "CREATE DATABASE $DB_NAME; use $DB_NAME"
-mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '$DB_USER'@'%'; FLUSH PRIVILEGES;"
-#mysql -u root $DB_NAME < /wpdb.sql
--- srcs/requirements/wordpress/setup.sh
+++
@@ -1,5 +0,0 @@
-
-/usr/sbin/php-fpm7.3 --nodaemonize
-
-#/etc/init.d/php7.3-fpm start
-#tail -f dev/null
